ab30240f00eed302838935ef5c3ce6c1ad753c4d
Trettel <aimee.trettel@gmhmacyd0pnjhd4.int.asurion.com>
2019-07-09 08:58:44 -0400

    Updated hangman

--- theSelfTaughtProgrammerChallenges/Chapter10/chapter10_game.py
+++ theSelfTaughtProgrammerChallenges/Chapter10/chapter10_game.py
@@ -1,5 +1,4 @@
 # Make hangman using everything we've learned so far
-# To Do: make leaderboard, keep track of wrong letters
 
 import random
 import csv
@@ -9,8 +8,9 @@ div = "\n------------------------------------------------"
 def hangman(word):
     win = False
     rletters = list(word)
+    wletters = list()
     board = ["_"] * len(word)
-    
+    checkPunc(rletters, board)
     stage1 = [
         "___________",
         "|     |    ",
@@ -99,6 +99,7 @@ def hangman(word):
         "    / \    ",
         "___/___\___"
         ]
+    lb = leaderboard()
     print(div)
     name = input("What is your name? ").capitalize()
     print(div)
@@ -108,14 +109,21 @@ def hangman(word):
     
     while wrong < 6:
         print("")
+        print("Already guessed: " + " ".join(wletters))
+        print("")
         print(" ".join(board))
         guess = input("Guess a letter: ").lower()
+        if len(guess) > 1:
+            print("*One letter at a time, please*")
+            continue
+        if guess in wletters:
+            print("*You already guessed that...*")
+            continue
+        else:
+            wletters.append(guess)
         if guess in rletters:
-            print("Good guess!")
-            while guess in rletters:
-                i = rletters.index(guess)
-                board[i] = guess
-                rletters[i] = "$"
+            print("*Good guess!*")
+            check(guess, rletters, board)
         else:
             wrong += 1
             stage = dead_stages[wrong -1]
@@ -128,8 +136,68 @@ def hangman(word):
             break
 
     if win == False:
-        print("Sorry, you lost! The word was " + word + ". Better luck next time!")
+        print("Sorry, you lost! It was '" + word + "'. Better luck next time!")
 
+    writeLeaderboard(lb, name, win)
+
+def checkPunc(rletters, board):
+    check(" ", rletters, board)
+    check("-", rletters, board)
+    check(",", rletters, board)
+    check(".", rletters, board)
+    check("!", rletters, board)
+    check("?", rletters, board)
+    check("'", rletters, board)
+    check(":", rletters, board)
+    check("(", rletters, board)
+    check(")", rletters, board)
+
+def check(char, rletters, board):
+    while char in rletters:
+        i = rletters.index(char)
+        board[i] = char
+        rletters[i] = "$"
+
+def leaderboard():
+    lb = dict()
+    with open("leaderboard.csv", "r") as f:
+        r = csv.DictReader(f)
+        print(div)
+        print("LEADERBOARD:")
+        linecount = 0
+        for row in r:
+            lb[row["name"]] = [int(row["wins"]), int(row["total"]), int(row["score"])]
+            if linecount == 0:
+                linecount += 1
+            print(f'{linecount}) {row["name"]} ({row["wins"]}/{row["total"]} wins) - Score: {row["score"]}')
+            linecount += 1
+    return lb
+
+def writeLeaderboard(lb, name, win):
+    if name in lb:
+        wins = lb[name][0]
+        total = lb[name][1]
+        score = lb[name][2]
+        if win:
+            wins += 1
+            score += 10
+        else:
+            score -= 5
+        total += 1
+        lb[name] = [wins, total, score]
+    else:
+        if win:
+            lb[name] = [1, 1, 10]
+        else:
+            lb[name] = [0, 1, -5]
+    def getScore(data):
+        return lb[data][2]
+    s = sorted(lb, key=getScore, reverse=True)
+    with open("leaderboard.csv", "w") as f:
+        w = csv.writer(f, delimiter=",")
+        w.writerow(["name","wins","total","score"])
+        for key in s:
+            w.writerow([key, lb[key][0], lb[key][1], lb[key][2]])
 
 def startGame():
     words = []
@@ -142,15 +210,18 @@ def startGame():
 
 def addWord():
     newWord = input("What word would you like to add? ").lower()
-    with open("words.csv", "a+", newline='') as f:
-        w = csv.writer(f, delimiter="\n")
-        w.writerow([newWord])
+    if len(newWord) < 5:
+        print("*Sorry, that word is too short*")
+    else:
+        with open("hardWords.csv", "a+", newline='') as f:
+            w = csv.writer(f, delimiter="\n")
+            w.writerow([newWord])
 
 def chooseWord():
     word = ""
     words = []
     print("\nWORD OPTIONS:")
-    with open("words.csv", "r") as f:
+    with open("hardWords.csv", "r") as f:
         reader = csv.reader(f, delimiter="\n")
         for i,row in enumerate(reader):
             print(row[0] + ":   \t" + str(i))
@@ -158,7 +229,10 @@ def chooseWord():
         print("Some new word that I specify: n")
         choice = input("Type the number of the word you want to choose: ").lower()
         if choice == "n":
-            word = input("What word do you want your opponent to solve? ").lower()
+            while len(word) < 5:
+                word = input("What word do you want your opponent to solve? ").lower()
+                if len(word) < 5:
+                    print("*That's not long enough*")
         else:
             word = words[int(choice)]
     i = 50
@@ -174,6 +248,7 @@ def options():
         print("MAIN MENU - HANGMAN")
         print("Start a game of hangman with the computer: s")
         print("Choose a word for your opponent: c")
+        print("Check the leaderboard: l")
         print("Add a new word: a")
         print("Quit: q")
         option = input("Type the letter of the option you want to choose: ").lower()
@@ -181,6 +256,8 @@ def options():
             startGame()
         elif option == "c":
             chooseWord()
+        elif option == "l":
+            leaderboard()
         elif option == "a":
             addWord()
         elif option != "q":
